9d31412a358a39a2985a4c46e3bd0a0a14235b21
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-03-10 11:20:56 -0500

    added Person Object

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,27 +1,56 @@
 // setup your IIFE (Immediately Invoked Function Expression)
-(function () {
-    
-"use strict";
+(function() {
 
-var myVariable = "hello";
-console.log("myVariable " + myVariable);
+    "use strict";
+    var output = "";
+    var firstParagraph = document.getElementById("firstParagraph");
 
-var myArray = [
-    "Tom Tsiliopoulos",
-    "1 Georgian Drive",
-    "Barrie",
-    "Ontario",
-    40,
-    true
-];
+    var Person = {
+        "age": undefined,
+        "name": "Tom",
+        "address": "1 Georgian Drive",
+        "city": "Barrie",
+        "province": "Ontario",
+        "familyNames": [
+            "Harrison",
+            "Marissa",
+            "Dino",
+            "Alexander"
+        ],
+        "sayHello":function(){
+            output += "<br><hr><br>";
+            output += Person.name + " says hello";
+        }
 
-console.log(myArray.slice(4, 1));
+    }; /* var Person = new Object();   */
 
-for(var index=0; index < myArray.length; index++) {
-    console.log(myArray[index]);
-}
+    // for every key in the Person object, loop...
+    for (var key in Person) {
 
+        // check if the key is the familyNames array
+        if (key === "familyNames") {
+           output += "<br>Family Names: <br>";
+            output += "<hr><br>";
+            output += "<ul>";
+            for (var index = 0; index < Person.familyNames.length; index++) {
+                output += "<li>" + Person.familyNames[index] + "</li>";
+            } // for loop
+            output += "</ul>";
+        } // if statement
+        
+        // check if the key is the sayHello Method
+        else if (key === "sayHello") {
+            Person.sayHello();
+        } // else if
+        
+        // for all other cases do the following...
+        else {
 
+            output += Person[key] + "<br>";
+        } // else statement
 
+    } // for in
+
+    firstParagraph.innerHTML = output;
 })();
 
