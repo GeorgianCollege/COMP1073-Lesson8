c05d567ed606ff8d6562036de4ef6eb6aeb6c747
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-03-10 11:59:22 -0500

    used AJAX to read person.txt file

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,52 +5,50 @@
     var output = "";
     var firstParagraph = document.getElementById("firstParagraph");
 
-    var Person = {
-        "age": undefined,
-        "name": "Tom",
-        "address": "1 Georgian Drive",
-        "city": "Barrie",
-        "province": "Ontario",
-        "familyNames": [
-            "Harrison",
-            "Marissa",
-            "Dino",
-            "Alexander"
-        ],
-        "sayHello":function(){
-            output += "<br><hr><br>";
-            output += Person.name + " says hello";
+    // Instantiate new xhr object
+    var request = new XMLHttpRequest();
+    request.open('GET', '../person.txt', true);
+    request.addEventListener('readystatechange', function() {
+        if (request.readyState === 4) {
+            var Person = {}; /* var Person = new Object();   */
+
+            Person = JSON.parse(request.responseText);
+            
+            Person.sayHello = function() {
+                output += "<br><hr><br>" + Person.name + " says hello";
+            }
+
+            // for every key in the Person object, loop...
+            for (var key in Person) {
+
+                // check if the key is the familyNames array
+                if (key === "familyNames") {
+                    output += "<br>Family Names: <br>";
+                    output += "<hr><br>";
+                    output += "<ul>";
+                    for (var index = 0; index < Person.familyNames.length; index++) {
+                        output += "<li>" + Person.familyNames[index] + "</li>";
+                    } // for loop
+                    output += "</ul>";
+                } // if statement
+                else if (key === "sayHello") {
+                    Person.sayHello();
+                }
+                
+                // for all other cases do the following...
+                else {
+
+                    output += Person[key] + "<br>";
+                } // else statement
+
+            } // for in
+
+            firstParagraph.innerHTML = output;
         }
+    });
 
-    }; /* var Person = new Object();   */
+    request.send();
 
-    // for every key in the Person object, loop...
-    for (var key in Person) {
 
-        // check if the key is the familyNames array
-        if (key === "familyNames") {
-           output += "<br>Family Names: <br>";
-            output += "<hr><br>";
-            output += "<ul>";
-            for (var index = 0; index < Person.familyNames.length; index++) {
-                output += "<li>" + Person.familyNames[index] + "</li>";
-            } // for loop
-            output += "</ul>";
-        } // if statement
-        
-        // check if the key is the sayHello Method
-        else if (key === "sayHello") {
-            Person.sayHello();
-        } // else if
-        
-        // for all other cases do the following...
-        else {
-
-            output += Person[key] + "<br>";
-        } // else statement
-
-    } // for in
-
-    firstParagraph.innerHTML = output;
 })();
 
